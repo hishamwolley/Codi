a49683cc42677d9a55ca9a089f83b3d8a052f1b9
Hisham Wolley <Hishamwolley@hotmail.com>
2021-06-25 22:53:45 +0300

    ..

--- cccc/src/index.js
+++
@@ -1,7 +0,0 @@
-// require("file-loader?name=[name].[ext]!./index.html");
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./App";
-import "./App.css";
-
-ReactDom.render(<App />, document.getElementById("root"));
